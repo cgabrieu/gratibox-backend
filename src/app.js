5b1469ddee115f93517b8be7c783ff249786f8e8
Carlos Gabriel <carlosgabrielpc@live.com>
2021-11-18 16:48:49 -0300

    feat(main): initial setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './setup.js';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/status', (req, res) => {
+  res.sendStatus(200);
+});
+
+export default app;
