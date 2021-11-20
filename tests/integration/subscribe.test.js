0cc00f7af09ee2485c21321007b5f62ad8054185
Carlos Gabriel <carlosgabrielpc@live.com>
2021-11-20 12:14:26 -0300

    test(subscribe): invalid inputs tests

--- tests/integration/subscribe.test.js
+++ tests/integration/subscribe.test.js
@@ -36,6 +36,7 @@ describe(`POST ${subscribeRoute}`, () => {
 
     expect(result.status).toEqual(201);
   });
+
   it('returns status 201 for valid inputs', async () => {
     const token = await createToken();
 
@@ -56,6 +57,7 @@ describe(`POST ${subscribeRoute}`, () => {
 
     expect(result.status).toEqual(201);
   });
+  
   it('returns status 409 if the user already has a subscription', async () => {
     const token = await createToken();
 
@@ -81,4 +83,118 @@ describe(`POST ${subscribeRoute}`, () => {
 
     expect(result.status).toEqual(409);
   });
+
+  it('returns status 400 when invalid inputs', async () => {
+    const token = await createToken();
+
+    let body = {
+      plan_type: faker.lorem.word,
+      day_month: '10',
+      receiving_options: [
+        { option_name: 'Chás' },
+        { option_name: 'Incensos' },
+        { option_name: 'Produtos Orgânicos' },
+      ],
+    };
+    let result = await request
+      .post(subscribeRoute)
+      .set('x-access-token', token)
+      .send(body);
+    expect(result.status).toEqual(400);
+
+    body = {
+      plan_type: 'Monthly',
+      day_month: faker.lorem.word,
+      receiving_options: [
+        { option_name: 'Chás' },
+        { option_name: 'Incensos' },
+        { option_name: 'Produtos Orgânicos' },
+      ],
+    };
+    result = await request
+      .post(subscribeRoute)
+      .set('x-access-token', token)
+      .send(body);
+    expect(result.status).toEqual(400);
+
+    body = {
+      plan_type: 'Monthly',
+      day_month: '10',
+      day_week: 'Sexta',
+      receiving_options: [
+        { option_name: 'Chás' },
+        { option_name: 'Incensos' },
+        { option_name: 'Produtos Orgânicos' },
+      ],
+    };
+    result = await request
+      .post(subscribeRoute)
+      .set('x-access-token', token)
+      .send(body);
+    expect(result.status).toEqual(400);
+
+    body = {
+      plan_type: 'Monthly',
+      day_week: 'Sexta',
+      receiving_options: [
+        { option_name: 'Chás' },
+        { option_name: 'Chás' },
+      ],
+    };
+    result = await request
+      .post(subscribeRoute)
+      .set('x-access-token', token)
+      .send(body);
+    expect(result.status).toEqual(400);
+
+    body = {
+      plan_type: 'Monthly',
+      day_week: 'Sexta',
+      receiving_options: [
+        { option_name: 'Chás' },
+        { option_name: 'Chás' },
+      ],
+    };
+    result = await request
+      .post(subscribeRoute)
+      .set('x-access-token', token)
+      .send(body);
+    expect(result.status).toEqual(400);
+
+    body = {
+      plan_type: 'Monthly',
+      receiving_options: [
+        { option_name: 'Chás' },
+        { option_name: 'Incensos' },
+      ],
+    };
+    result = await request
+      .post(subscribeRoute)
+      .set('x-access-token', token)
+      .send(body);
+    expect(result.status).toEqual(400);
+
+    body = {
+      plan_type: 'Monthly',
+      day_week: 'Sexta',
+      receiving_options: [
+      ],
+    };
+    result = await request
+      .post(subscribeRoute)
+      .set('x-access-token', token)
+      .send(body);
+    expect(result.status).toEqual(400);
+
+    body = {
+      plan_type: 'Monthly',
+      day_week: 'Sexta',
+    };
+    result = await request
+      .post(subscribeRoute)
+      .set('x-access-token', token)
+      .send(body);
+    expect(result.status).toEqual(400);
+  });
+
 });
