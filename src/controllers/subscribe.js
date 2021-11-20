f339d7f6fc75ca76a980f42b875b5b4080516554
Carlos Gabriel <carlosgabrielpc@live.com>
2021-11-20 00:39:36 -0300

    feat(schema): joi schema for subscribe

--- src/controllers/subscribe.js
+++ src/controllers/subscribe.js
@@ -1,13 +1,31 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import connection from '../database/database.js';
-import { signInSchema } from '../schemas/usersSchemas.js';
+import { subcribeSchema } from '../schemas/subscriptionSchemes.js';
 
 export default async function subscribe(req, res) {
   try {
-    const { } = req.body;
+    const {
+      plan_type: planType,
+      day_month: dayMonth,
+      day_week: dayWeek,
+      receiving_options: receivingOptions,
+    } = req.body;
 
-    return res.status(401).send('E-mail ou senha inválidos');
+    const validationResult = subcribeSchema.validate(req.body, { abortEarly: false });
+
+    if (validationResult.error) {
+      return res.status(400).send(validationResult.error.details[0].message);
+    }
+
+    return res.send({
+      planType,
+      dayMonth,
+      dayWeek,
+      receivingOptions,
+    });
+
+    // return res.status(401).send('E-mail ou senha inválidos');
   } catch (error) {
     console.log(error);
     return res.status(500);
---
+++ src/schemas/subscriptionSchemes.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/prefer-default-export */
+import joi from 'joi';
+
+const subcribeSchema = joi
+  .object({
+    plan_type: joi.string().valid('Monthly', 'Weekly').required(),
+    day_month: joi.string().valid('1', '10', '20'),
+    day_week: joi.string().valid('Segunda', 'Quarta', 'Sexta'),
+    receiving_options: joi.array()
+      .items(
+        joi.object({
+          option_name: joi.string().valid('Chás'),
+        }),
+        joi.object({
+          option_name: joi.string().valid('Incensos'),
+        }),
+        joi.object({
+          option_name: joi.string().valid('Produtos Orgânicos'),
+        }),
+      )
+      .unique()
+      .min(1)
+      .required(),
+  })
+  .xor('day_month', 'day_week');
+
+export { subcribeSchema };
